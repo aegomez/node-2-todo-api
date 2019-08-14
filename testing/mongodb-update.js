c3fd3b00462993cc557d8a0485be5b95cf4533ce
Adrian Gomez <aegomez@tekmexico.com>
2019-08-13 17:32:57 -0700

    Add update script

---
+++ testing/mongodb-update.js
@@ -0,0 +1,58 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+const { log } = require('./lib');
+const { URL } = require('../mongo-url');
+
+MongoClient.connect(
+  URL,
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      return log('Unable to connect to MongoDB server', err);
+    }
+    log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    /* Todos */
+    // db.collection('Todos').findOneAndUpdate(
+    //   { _id: new ObjectID("5d533c666e3275932ade0def") },
+    //   {
+    //     $set: {
+    //       completed: true
+    //     }
+    //   },
+    //   { returnOriginal: false }
+    // )
+    // .then(result => {
+    //   log('Document successfully updated', result);
+    // })
+    // .catch(err => {
+    //   log('Unable to update document', err);
+    // });
+    
+    // client.close();
+
+    /* Users */
+    db.collection('Users').findOneAndUpdate(
+      { _id: 1 },
+      {
+        $set: {
+          name: 'Ed'
+        },
+        $inc: {
+          age: -2
+        }
+      },
+      { returnOriginal: false }
+    )
+    .then(result => {
+      log('Document successfully updated', result);
+    })
+    .catch(err => {
+      log('Unable to update document', err);
+    });
+    
+    client.close();
+  }
+);
