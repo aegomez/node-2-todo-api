864f9db14d40ed8c9df064be280b227975f4e998
Adrian Gomez <aegomez@tekmexico.com>
2019-08-16 14:46:45 -0700

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // replace URL as needed
-const {URL} = require('../../mongo-url');
+const URL = process.env.MONGODB_URI ||
+  require('../../mongo-url').URL;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(URL, { useNewUrlParser: true });
