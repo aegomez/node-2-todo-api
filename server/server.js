efc4be2f4d0a273544613647408030a078b35e61
Adrian Gomez <aegomez@tekmexico.com>
2019-08-16 17:46:01 -0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb').ObjectId;
 
+const pick = require('lodash/pick');
+const isBoolean = require('lodash/isBoolean');
+
 // local imports
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -55,6 +58,33 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id)
+  .then(todo => todo ?
+    res.send({ todo }) :
+    res.status(404).send('Not found'))
+  .catch(err => res.status(400).send('DB error'));
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send('Not valid');
+  }
+
+  let body = pick(req.body, [ 'text', 'completed' ]);
+
+  if (isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate( id,
+    { $set: body },
+    { new: true }
+  )
   .then(todo => todo ?
     res.send({ todo }) :
     res.status(404).send('Not found'))
